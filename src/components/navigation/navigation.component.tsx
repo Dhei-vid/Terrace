049c93a20f902a334543ccd95c44fe6230c22006
David Etuk <djetuk@gmail.com>
2023-02-05 22:46:21 +0100

    fix:used the correct logo for the nav.

--- src/components/navigation/navigation.component.tsx
+++ src/components/navigation/navigation.component.tsx
@@ -1,10 +1,16 @@
 import logo from "../../images/logo.svg";
+import hamburger from "../../images/icon-hamburger.svg";
 
 const Navigation = () => {
   return (
     <>
       <ul className="flex space-x-8 justify-items-center items-center px-7 absolute top-10 left-5 text-white">
-        <li className="text-xl font-extrabold cursor-pointer mr-5">room</li>
+        <li className="cursor-pointer items-center md:hidden">
+          <img src={hamburger} alt="hamburger icon" />
+        </li>
+        <li className="cursor-pointer mr-5 items-center">
+          <img src={logo} alt="logo" />
+        </li>
         <li className="cursor-pointer">home</li>
         <li className="cursor-pointer">shop</li>
         <li className="cursor-pointer">about</li>
