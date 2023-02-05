889cdd8ce77921679dceffee5fba168b95ef3ce0
David Etuk <djetuk@gmail.com>
2023-02-05 23:46:37 +0100

    feat:added the hamburger menu (not functional)

--- src/components/navigation/navigation.component.tsx
+++ src/components/navigation/navigation.component.tsx
@@ -4,17 +4,17 @@ import hamburger from "../../images/icon-hamburger.svg";
 const Navigation = () => {
   return (
     <>
-      <ul className="flex space-x-8 justify-items-center items-center px-7 absolute top-10 left-5 text-white">
+      <ul className="flex space-x-28 md:space-x-8 justify-items-center items-center px-7 absolute top-10 left-5 text-white">
         <li className="cursor-pointer items-center md:hidden">
           <img src={hamburger} alt="hamburger icon" />
         </li>
-        <li className="cursor-pointer mr-5 items-center">
+        <li className="md:block cursor-pointer mr-5 mt-1 items-center">
           <img src={logo} alt="logo" />
         </li>
-        <li className="cursor-pointer">home</li>
-        <li className="cursor-pointer">shop</li>
-        <li className="cursor-pointer">about</li>
-        <li className="cursor-pointer">contact</li>
+        <li className="md:block cursor-pointer hidden">home</li>
+        <li className="md:block cursor-pointer hidden">shop</li>
+        <li className="md:block cursor-pointer hidden">about</li>
+        <li className="md:block cursor-pointer hidden">contact</li>
       </ul>
     </>
   );
