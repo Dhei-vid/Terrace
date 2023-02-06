f0dff702d0282564b1b891bb5fa85ae3f4dc23ed
David Etuk <djetuk@gmail.com>
2023-02-07 00:26:36 +0100

    feat:styled the mobile component for pg2 & 3

--- src/components/page2/page2.component.tsx
+++ src/components/page2/page2.component.tsx
@@ -4,32 +4,41 @@ import Hero from "../bottom/hero.component";
 import Navigation from "../navigation/navigation.component";
 
 import image2 from "../../images/desktop-image-hero-2.jpg";
+import image22 from "../../images/mobile-image-hero-2.jpg";
 
 const Page2 = () => {
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-7">
-        <div className="col-span-4 relative">
-          <Navigation />
-          <img
-            className="h-100 w-full"
-            src={image2}
-            alt="home"
-            loading="lazy"
-          />
-        </div>
-        <div className="col-span-3 px-10 pt-8 md:px-28 justify-items-center md:relative">
-          <h2 className="text-3xl font-bold pb-6">
-            We are available all across the globe
-          </h2>
-          <p className="subpixel-antialiased text-gray-500 text-xs">
-            With stores all over the world, it's easy to find furniture for your
-            home or place of business. Locally, we're in most cities throughout
-            the country. Find the branch nearest you using our store locator.
-            Any questions? Don't hesitate to contact us today.
-          </p>
-          <ShopNowButton />
-          <Button prev={`/`} next={`/page3`} />
+      <div className="relative">
+        <Button prev={`/`} next={`/page3`} />
+        <div className="grid grid-cols-1 md:grid-cols-7">
+          <div className="col-span-4">
+            <Navigation />
+            <img
+              className="hidden h-100 w-full md:block"
+              src={image2}
+              alt="home"
+              loading="lazy"
+            />
+            <img
+              className="h-100 w-full md:hidden"
+              src={image22}
+              alt="home"
+              loading="lazy"
+            />
+          </div>
+          <div className="col-span-3 px-10 pt-8 md:px-28 justify-items-center">
+            <h2 className="text-3xl font-bold pb-6">
+              We are available all across the globe
+            </h2>
+            <p className="subpixel-antialiased text-gray-500 text-xs">
+              With stores all over the world, it's easy to find furniture for
+              your home or place of business. Locally, we're in most cities
+              throughout the country. Find the branch nearest you using our
+              store locator. Any questions? Don't hesitate to contact us today.
+            </p>
+            <ShopNowButton />
+          </div>
         </div>
       </div>
       <Hero />
--- src/components/page3/page3.component.tsx
+++ src/components/page3/page3.component.tsx
@@ -4,33 +4,42 @@ import Hero from "../bottom/hero.component";
 import Navigation from "../navigation/navigation.component";
 
 import image3 from "../../images/desktop-image-hero-3.jpg";
+import image33 from "../../images/mobile-image-hero-3.jpg";
 
 const Page3 = () => {
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-7">
-        <div className="col-span-4 relative">
-          <Navigation />
-          <img
-            className="h-100 w-full"
-            src={image3}
-            alt="home"
-            loading="lazy"
-          />
-        </div>
-        <div className="col-span-3 px-10 md:px-28 pt-8 justify-items-center md:relative">
-          <h2 className="text-3xl subpixel-antialiased font-bold pb-6">
-            Manufactured with the best materials
-          </h2>
-          <p className="text-gray-500 text-xs">
-            Our modern furniture store provide a high level of quality. Our
-            company has invested in advanced technology to ensure that every
-            product is made as perfect and as consistent as possible. With three
-            decades of experience in this industry, we understand what customers
-            want for their home and office.
-          </p>
-          <ShopNowButton />
-          <Button prev={`/page2`} next={`/page3`} />
+      <div className="relative">
+        <div className="grid grid-cols-1 md:grid-cols-7">
+          <div className="col-span-4">
+            <Navigation />
+            <img
+              className="hidden h-100 w-full md:block"
+              src={image3}
+              alt="home"
+              loading="lazy"
+            />
+            <img
+              className="h-100 w-full md:hidden"
+              src={image33}
+              alt="home"
+              loading="lazy"
+            />
+          </div>
+          <div className="col-span-3 px-10 md:px-28 pt-8 justify-items-center">
+            <h2 className="text-3xl subpixel-antialiased font-bold pb-6">
+              Manufactured with the best materials
+            </h2>
+            <p className="text-gray-500 text-xs">
+              Our modern furniture store provide a high level of quality. Our
+              company has invested in advanced technology to ensure that every
+              product is made as perfect and as consistent as possible. With
+              three decades of experience in this industry, we understand what
+              customers want for their home and office.
+            </p>
+            <ShopNowButton />
+            <Button prev={`/page2`} next={`/page3`} />
+          </div>
         </div>
       </div>
       <Hero />
