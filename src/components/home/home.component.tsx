db8abf8f5bafa6fa714183394c08c0970828c0d7
David Etuk <djetuk@gmail.com>
2023-02-07 00:02:39 +0100

    feat: styled the mobile responsive component for the home page

--- src/components/home/home.component.tsx
+++ src/components/home/home.component.tsx
@@ -1,4 +1,5 @@
 import image1 from "../../images/desktop-image-hero-1.jpg";
+import image11 from "../../images/mobile-image-hero-1.jpg";
 
 import Navigation from "../navigation/navigation.component";
 import ShopNowButton from "../button/shopButton.component";
@@ -8,29 +9,37 @@ import Hero from "../bottom/hero.component";
 const Home = () => {
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-7">
-        <div className="col-span-4 relative">
-          <Navigation />
-          <img
-            className="h-100 w-full"
-            src={image1}
-            alt="home"
-            loading="lazy"
-          />
-        </div>
-        <div className="col-span-3 px-10 md:px-28 pt-8 justify-items-center md:relative">
-          <h2 className="text-3xl font-bold pb-6">
-            Discover innovative ways to decorate
-          </h2>
-          <p className="subpixel-antialiased text-gray-500 text-xs">
-            We provide unmatched quality, comfort, and style for property owners
-            across the country. Our experts combine form and function into
-            bringing your vision to life. Create a room in your own style with
-            our collection and make your property a reflection of you and what
-            you love.
-          </p>
-          <ShopNowButton />
-          <Button prev={`/`} next={`/page2`} />
+      <div className="relative">
+        <Button prev={`/`} next={`/page2`} />
+        <div className="grid grid-cols-1 md:grid-cols-7">
+          <div className="col-span-4">
+            <Navigation />
+            <img
+              className="hidden h-100 w-full md:block"
+              src={image1}
+              alt="home"
+              loading="lazy"
+            />
+            <img
+              className="h-100 w-full md:hidden"
+              src={image11}
+              alt="home"
+              loading="lazy"
+            />
+          </div>
+          <div className="col-span-3 px-10 md:px-28 pt-8 justify-items-center">
+            <h2 className="text-3xl font-bold pb-6">
+              Discover innovative ways to decorate
+            </h2>
+            <p className="subpixel-antialiased text-gray-500 text-xs">
+              We provide unmatched quality, comfort, and style for property
+              owners across the country. Our experts combine form and function
+              into bringing your vision to life. Create a room in your own style
+              with our collection and make your property a reflection of you and
+              what you love.
+            </p>
+            <ShopNowButton />
+          </div>
         </div>
       </div>
       <Hero />
