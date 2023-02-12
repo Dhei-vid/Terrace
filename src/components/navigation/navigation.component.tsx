cc1a9d02f5e0d51d34024c891f222892c43ad470
David Etuk <djetuk@gmail.com>
2023-02-12 21:18:14 +0100

    feat: adding animation to navigation menu

--- src/components/navigation/navigation.component.tsx
+++ src/components/navigation/navigation.component.tsx
@@ -1,5 +1,6 @@
 import logo from "../../images/logo.svg";
 import hamburger from "../../images/icon-hamburger.svg";
+import close from "../../images/icon-close.svg";
 
 const Navigation = () => {
   return (
@@ -7,14 +8,23 @@ const Navigation = () => {
       <ul className="flex space-x-28 md:space-x-8 justify-items-center items-center px-7 absolute top-10 left-5 text-white">
         <li className="cursor-pointer items-center md:hidden">
           <img src={hamburger} alt="hamburger icon" />
+          <img src={close} alt="close" />
         </li>
         <li className="md:block cursor-pointer mr-5 mt-1 items-center">
           <img src={logo} alt="logo" />
         </li>
-        <li className="md:block cursor-pointer hidden">home</li>
-        <li className="md:block cursor-pointer hidden">shop</li>
-        <li className="md:block cursor-pointer hidden">about</li>
-        <li className="md:block cursor-pointer hidden">contact</li>
+        <li className="transition ease-in-out delay-150 md:block cursor-pointer hidden hover:scale-110">
+          home
+        </li>
+        <li className="transition ease-in-out delay-150 md:block cursor-pointer hidden hover:scale-110">
+          shop
+        </li>
+        <li className="transition ease-in-out delay-150 md:block cursor-pointer hidden hover:scale-110">
+          about
+        </li>
+        <li className="transition ease-in-out delay-150 md:block cursor-pointer hidden hover:scale-110">
+          contact
+        </li>
       </ul>
     </>
   );
